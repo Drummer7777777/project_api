fa5e7ee9592937495390a93905bf5c6c8d0c1583
Drummer7777777 <73654451+Drummer7777777@users.noreply.github.com>
2021-04-13 19:42:36 +0500

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest, json
-#from threading import *
-
-@pytest.fixture(scope='module')
-def data_base():
-    with open('data_base.json', encoding='utf-8') as f:
-        users=json.load(f)
-        return(users)
