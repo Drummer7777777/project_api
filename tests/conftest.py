9f1fc79727b8cfa5f70f8102dab7c5fbb87fb7d8
Drummer7777777 <73654451+Drummer7777777@users.noreply.github.com>
2021-04-15 18:07:46 +0500

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest, json
+#from threading import *
+
+@pytest.fixture(scope='function')
+def data_base():
+    f=open('data_base.json', encoding='utf-8')
+    global users
+    users=json.load(f)
+    print(users)
+    yield users
+    f.close()
