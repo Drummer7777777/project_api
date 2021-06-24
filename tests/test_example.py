d62f91ef8ecc3f028fd6a058088e700f55a2ad46
Drummer7777777 <newlogan95@mail.ru>
2021-06-24 13:27:46 +0500

    test

--- tests/test_example.py
+++ tests/test_example.py
@@ -177,5 +177,7 @@ class TestUsers():
             logs(func_name,passed)
         else:
             logs(func_name,failed,'В ответе не все совпадения')
-        assert r==str(all_coincidence)
+        assaert r==str(all_coincidence)
         
+    def test_test(self):
+        pass
